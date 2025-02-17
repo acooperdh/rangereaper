028b6cdf1a94e6ce6d86c2bb0afe46784cc30c04
acooperdh <acooper@dal.ca>
2025-02-17 18:26:55 -0500

    setting up for storing all ranges in memory - want to move to storing ranges in CSVs

--- map_to_ranges.py
+++ map_to_ranges.py
@@ -1,8 +1,6 @@
 # Contains the direct file paths from the current working directory to the hand ranges
 import pathlib
 import os
-def get_range_file_name(position):
-    return
 
 ACTION_NAMES = ["RFI", "RAISE", "3BET", "4BET", "4BET_ALLIN", "5BET_ALLIN"]
 
@@ -11,13 +9,51 @@ POSITIONS = ["LJ", "HJ", "CO", "BTN", "SB", "BB"]
 current_dir = os.path.dirname(__file__)
 range_dir = "CASH_6MAX_100"
 BB_RANGES = pathlib.Path(f'{current_dir}/{range_dir}/{POSITIONS[0]}/{ACTION_NAMES[0]}/{POSITIONS[0]}_{ACTION_NAMES[0]}.html')
-print(BB_RANGES)
 
 
 
 # f = open(BB_RANGES, "r")
 # print(f.read())
 
+'''
+Folders are structured as follows
+Position 
+    Action
+        Ranges
+Range files use the following naming structure
+POSITION_VS_VILLAN_ACTION.html
+where Villan is the other player involved in the hand. 
+Action should be the same as the action folder name and 
+position should be the same as the position folder name
+'''
+files = []
 for position in POSITIONS:
     for action in ACTION_NAMES:
-        print(f'{position}/{action}')
+        temp_path = ""
+        if action == "RFI":
+            temp_path = f'{current_dir}/{range_dir}/{position}/{action}/{position}_{action}.html'
+            folder_exists = pathlib.Path(temp_path).exists()
+            if folder_exists:
+                temp_obj = {
+                    "position": position,
+                    "action": action,
+                    "villan": "NA",
+                    "file": temp_path
+
+                }
+                files.append(temp_obj)
+        else:
+            for p in POSITIONS: 
+                temp_path = f'{current_dir}/{range_dir}/{position}/{action}/{position}_VS_{p}_{action}.html'
+                folder_exists = pathlib.Path(temp_path).exists()
+                if folder_exists:
+                    temp_obj = {
+                        "position": position,
+                        "action": action,
+                        "villan": p,
+                        "file": temp_path
+                    }
+                    files.append(temp_obj)
+for x in files:
+    print(x)
+
--- scrape_range_charts.py
+++ scrape_range_charts.py
@@ -6,11 +6,11 @@ import random
 fold_regex = re.compile("^Fold=")
 call_regex = re.compile("^Call=")
 raise_regex = re.compile("^Raise \d*")
-three_bet_regex = re.compile("^3-bet \d+bb=.*")
-four_bet_raise_regex = re.compile("4-bet \d+bb=.*")
-four_bet_all_in_regex = re.compile("4-bet All-in=")
-five_bet_raise_regex = re.compile("5-bet \d+bb=.*")
-five_bet_all_in_regex = re.compile("5-bet All-in=")
+three_bet_regex = re.compile("^3-bet \d*")
+four_bet_raise_regex = re.compile("4-bet \d*")
+four_bet_all_in_regex = re.compile("4-bet All-in=*")
+five_bet_raise_regex = re.compile("5-bet \d*")
+five_bet_all_in_regex = re.compile("5-bet All-in=*")
 comments_regex = re.compile("<!--|-->")
 source = "CASH_6MAX_100_LJ_RFI.html"
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -34,33 +34,86 @@ hands = {}
 for row in chart_columns:
     hand = row.attrs['data-handid']
     temp = {}
+    can_call = row.find("div", {"class": call_regex})
     can_raise = row.find("div", {"class": raise_regex})
     can_fold = row.find("div", {"class": fold_regex})
-    print(f'hand {hand}')
-    print(f'can_raise {can_raise}')
-    print(f'{type(can_raise)}')
-    print(f'can_raise is None = {can_raise is None}')
-    print(f'can_fold {can_fold}')
-    print(f'{type(can_fold)}')
-    print(f'can_fold is None = {can_fold is None}')
+    can_3bet = row.find("div", {"class": three_bet_regex})
+    can_4bet = row.find("div", {"class": four_bet_raise_regex})
+    can_4bet_allin = row.find("div", {"class": four_bet_all_in_regex})
+    can_5bet = row.find("div", {"class": five_bet_raise_regex})
+    can_5bet_allin = row.find("div", {"class": five_bet_all_in_regex})
+
     if can_raise is not None:
         raise_info = can_raise.attrs['class'][1].split("=")
         value = raise_info[0]
-        freq = float(raise_info[1])
+        freq = raise_info[1]
         temp['raise'] = {
             "value": value,
             "freq": freq
         }
     else: 
         temp['raise'] = {
-            "value": 0,
-            "freq": 0
+            "value": "0",
+            "freq": "0"
         }
     if can_fold is not None:
-        fold_info = can_fold.attrs['class'][0].split("=")
-        temp['fold'] = {"freq": float(fold_info[1])}
+        fold_info = can_fold.attrs['class'][0].split("=")[1]
+        temp['fold'] = {"freq": fold_info}
     else:
-        temp['fold'] = {"freq": 0}
+        temp['fold'] = {"freq": "0"}
+    if can_call is not None:
+        call_info = can_call.attrs['class'].split["="][1]
+        temp['call'] =  {
+            "freq": call_info
+        }
+    else: 
+        temp['call'] = {
+            "freq": "0"
+        }
+    if can_4bet is not None:
+        four_bet_info = can_4bet.attrs['class'].split("=")
+        value = four_bet_info[0].split(" ")[1]
+        freq = four_bet_info[1]
+        temp['4bet'] = {
+            "freq":  freq,
+            "value": value
+        }
+    else: 
+        temp['4bet'] = {
+            "freq": "0"
+        }
+    if can_4bet_allin is not None:
+        four_bet_allin_info = can_4bet_allin.attrs['class'].split("=")[1]
+        temp['4bet_allin'] = {
+            "freq": four_bet_allin_info
+        }
+    else:
+        temp['4bet_allin'] = {
+            "freq": "0"
+        }
+    if can_5bet is not None:
+        five_bet_info = can_5bet.attrs['class'].split("=")
+        value = five_bet_info[0].split(" ")[1]
+        freq = five_bet_info[1] 
+        temp['5bet'] = {
+            "freq": freq,
+            "value": value
+        }
+    else:
+        temp['5bet'] = {
+            "freq": "0"
+        }
+    if can_5bet_allin is not None:
+        four_bet_allin_info = can_5bet_allin.attrs['class'].split("=")[1]
+        temp['5bet_allin'] = {
+            "freq": four_bet_allin_info
+        }
+    else:
+        temp['5bet_allin'] = {
+            "freq": "0"
+        }
+
+    
     hands[hand] = temp
 print(hands)
 print(hands['54s'])
